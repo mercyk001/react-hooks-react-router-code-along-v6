4942e4ab6ee57250ede8c3cbdb5d7a14be6cf9a7
mercy chepkemoi koech <koechmercyk@gmail.com>
2025-04-16 23:26:54 +0300

    client side

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+function About () {
+    return (
+        <>
+        <header>
+
+        </header> 
+
+        <main>
+            <h1>This is my About component</h1>
+                </main>  
+            
+            
+            
+            </>
+    );
+};
+export default About;
